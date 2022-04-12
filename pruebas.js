69bd3f172dac8552f076ce5015a62d535927deb1
yoryao <jorgejoaquinrivera@gmail.com>
2022-04-12 00:40:53 -0300

    working on modal for detail

--- pruebas.js
+++ pruebas.js
@@ -1,7 +1,9 @@
+//al cargar ejecutar la funcion de solicitar productos a la api, y mostrar.
 document.addEventListener("DOMContentLoaded", () => {
   solicitarData();
 });
 
+//capturar productos o por categoria
 const solicitarData = async (categoria) => {
   let url = "https://fakestoreapi.com/products/category/" + categoria;
 
@@ -15,14 +17,11 @@ const solicitarData = async (categoria) => {
     ponerCards(data);
   } catch (error) {
   } finally {
-    loader(false);
+    //loader(false);
   }
 };
 
-function verDetalle(id) {
-  console.log("funciona el detalle." + id);
-}
-
+//mostrar los productos.
 const ponerCards = (data) => {
   const clearTemplate = document.getElementById("card-dinamicas");
   clearTemplate.innerHTML = `<section class="row" id="card-dinamicas"></section>`;
@@ -38,7 +37,7 @@ const ponerCards = (data) => {
     clone.getElementById("card-text").textContent = item.description;
     clone.getElementById("card-img").setAttribute("src", item.image);
     clone.getElementById("detailButton").addEventListener("click", function () {
-      verDetalle(item.id);
+      verDetalle(item.id, item);
     });
     fragment.appendChild(clone);
   });
@@ -46,14 +45,47 @@ const ponerCards = (data) => {
   cards.appendChild(fragment);
 };
 
-const loader = (state) => {
-  const spinner = document.getElementById("spinner");
+// FUNCION MOSTRAR MODAL con detalle del producto.
+function verDetalle(id, item) {
 
-  if (state) {
-    console.log("funciona add");
-    spinner.classList.remove("d-none");
-  } else {
-    console.log("funciona remove");
-    spinner.classList.remove("d-none");
-  }
-};
+const titulo = document.getElementById("h1Modal")
+const description = document.getElementById("h2Modal")
+titulo.innerText = item.title
+description.innerText = item.description
+
+
+
+
+
+
+
+
+
+
+
+
+
+  const modal = document.getElementById("modalContainer");
+  modal.classList.add("mostrar");
+
+  console.log("funciona el detalle." + id);
+}
+
+//ocultar modal
+function ocultarDetalle() {
+  const modal = document.getElementById("modalContainer");
+  modal.classList.remove("mostrar");
+  console.log("Cerrar Modal");
+}
+
+// const loader = (state) => {
+//   const spinner = document.getElementById("spinner");
+
+//   if (state) {
+//     console.log("funciona add");
+//     spinner.classList.remove("d-none");
+//   } else {
+//     console.log("funciona remove");
+//     spinner.classList.remove("d-none");
+//   }
+// };
