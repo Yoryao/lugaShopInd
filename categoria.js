a0b4bf38ee6b63f94118481e60522392fa51a8e5
yoryao <jorgejoaquinrivera@gmail.com>
2022-04-14 01:21:29 -0300

    load & show productos working

--- categoria.js
+++ categoria.js
@@ -1,16 +1,17 @@
 console.log("Productos Funcionando.");
 
+//1) llamo a las categorias.
 window.onload = function () {
   console.log("On Load Working");
   loadCategorias();
 };
 
-//solicitar las categorias
+//2) capturo las categorias
 const loadCategorias = async () => {
   try {
     const res = await fetch("categorias.json");
     const data = await res.json();
-    loadData(data);
+    mostrarCategorias(data);
   } catch (error) {
     console.log(error);
   } finally {
@@ -18,50 +19,8 @@ const loadCategorias = async () => {
   }
 };
 
-function mostrarProductos(id) {
-  console.log("Se ha clickeado categoria " + id);
-  document.location = "productos.html";
-  loadProductos();
-}
-
-const loadProductos =  () => {
-
-  console.log("hola")
-
-  // const cardCategoria = document.getElementById("categoriasContainer");
-  // const template = document.getElementById("template").content;
-  // const fragment = document.createDocumentFragment();
-
-  // data.forEach((item) => {
-  //   const clone = template.cloneNode(true);
-
-  //   clone.getElementById("categoryButton").style.backgroundImage = `url('${item.img}')`;
-  //   clone.getElementById("categoryP").textContent = item.nombre;
-  //   clone
-  //     .getElementById("categoryButton")
-  //     .addEventListener("click", function () {
-  //       mostrarProductos(item.nombre, item.id);
-  //     });
-
-  //   fragment.appendChild(clone);
-  // });
-  // cardCategoria.appendChild(fragment);
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const loadData = async (data) => {
+//3) muestro las categorias
+const mostrarCategorias = async (data) => {
   const cardCategoria = document.getElementById("categoriasContainer");
   const template = document.getElementById("template").content;
   const fragment = document.createDocumentFragment();
@@ -74,10 +33,58 @@ const loadData = async (data) => {
     clone
       .getElementById("categoryButton")
       .addEventListener("click", function () {
-        mostrarProductos(item.nombre, item.id);
+        elegirCategoria(item.nombre, item.id);
       });
 
     fragment.appendChild(clone);
   });
   cardCategoria.appendChild(fragment);
 };
+
+//4) elijo la categoria.
+function elegirCategoria(id) {
+  console.log("Se ha clickeado categoria " + id);
+  loadProductos( id );
+}
+
+//5) cargar productos.
+const loadProductos = async ( id ) => {
+  try {
+    const res = await fetch("productos.json");
+    const data = await res.json();
+    mostrarProductos(data);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    console.log("finally del loadProductos.");
+  }
+};
+
+//6) mostrar Productos
+const mostrarProductos = async ( data ) => {
+   
+  
+   const cardProductos = document.getElementById("productosContainer");
+   const templateProducto = document.getElementById("templateProductos").content;
+   const fragment = document.createDocumentFragment();
+  console.log(data)
+  data.forEach((item) => {
+    console.log((item))
+    const clone = templateProducto.cloneNode(true);
+  
+    clone.getElementById("productosImage").setAttribute("src" , item.images[0]);
+    clone.getElementById("productosImage").setAttribute("alt" , `Photo of ${item.title}`);
+    clone.getElementById("productosTitle").textContent = item.title;
+    clone.getElementById("productosInfo").textContent = item.description;
+    clone.getElementById("addButton")
+    .addEventListener("click", function () {
+      console.log(`se eligio el producto: ${item.id}`)})
+      //       mostrarProductos(item.nombre, item.id);
+      fragment.appendChild(clone);
+   });
+     cardProductos.appendChild(fragment);
+  }
+
+
+// <button id="addButton" class="section__add__button"> Agregar
+// </button>
