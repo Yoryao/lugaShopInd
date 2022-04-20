4e846d81de8884ff961b7ffc70372d620c68d020
yoryao <jorgejoaquinrivera@gmail.com>
2022-04-20 00:48:18 -0300

    cart finished - continue with sending productos through mail

--- categoria.js
+++ categoria.js
@@ -1,27 +1,24 @@
-
 //VALIDAR NO SUMAR ITEMS YA EXISTENTES.
 //realizar toast para producto existente en el carrito
 console.log("Productos Funcionando.");
 
-
 //0)DECLARO VARIABLES A UTILIZAR
 let carrito = [
   {
-    "posicion": 1,
-    "nombre": "Women ",
-    "id": 58
-},
-{
-  "posicion": 2,
-  "nombre": "Heel",
-  "id": 48
-},
-{
-  "posicion": 3,
-  "nombre": "Strip",
-  "id": 47
-}
-
+    posicion: 1,
+    nombre: "Women ",
+    id: 58,
+  },
+  {
+    posicion: 2,
+    nombre: "Heel",
+    id: 48,
+  },
+  {
+    posicion: 3,
+    nombre: "Strip",
+    id: 47,
+  },
 ];
 
 //1) llamo a las categorias.
@@ -48,29 +45,29 @@ const mostrarCategorias = async (data) => {
   const cardCategoria = document.getElementById("categoriasContainer");
   const template = document.getElementById("template").content;
   const fragment = document.createDocumentFragment();
-  
+
   data.forEach((item) => {
     const clone = template.cloneNode(true);
-    
+
     clone.getElementById(
       "categoryButton"
-      ).style.backgroundImage = `url('${item.img}')`;
-      clone.getElementById("categoryP").textContent = item.nombre;
-      clone
-    .getElementById("categoryButton")
-    .addEventListener("click", function () {
-      elegirCategoria(item.nombre, item.id);
+    ).style.backgroundImage = `url('${item.img}')`;
+    clone.getElementById("categoryP").textContent = item.nombre;
+    clone
+      .getElementById("categoryButton")
+      .addEventListener("click", function () {
+        elegirCategoria(item.nombre, item.id);
       });
 
-      fragment.appendChild(clone);
-    });
-    cardCategoria.appendChild(fragment);
+    fragment.appendChild(clone);
+  });
+  cardCategoria.appendChild(fragment);
 };
 
 //4) elijo la categoria.
 function elegirCategoria(id) {
   console.log("Se ha clickeado categoria " + id);
-  
+
   loadProductos(id);
 }
 
@@ -79,7 +76,7 @@ const loadProductos = async (id) => {
   try {
     const res = await fetch("productos.json");
     const data = await res.json();
-    
+
     let filtrado = data.filter((item) => item.category == id);
     mostrarProductos(filtrado);
   } catch (error) {
@@ -93,16 +90,16 @@ const loadProductos = async (id) => {
 const mostrarProductos = async (data) => {
   const cardProductos = document.getElementById("productosContainer");
   cardProductos.innerHTML = "";
-  
+
   const templateProducto = document.getElementById("templateProductos").content;
   const fragment = document.createDocumentFragment();
   data.forEach((item) => {
     const clone = templateProducto.cloneNode(true);
-    
+
     clone.getElementById("productosImage").setAttribute("src", item.images[0]);
     clone
-    .getElementById("productosImage")
-    .setAttribute("alt", `Photo of ${item.title}`);
+      .getElementById("productosImage")
+      .setAttribute("alt", `Photo of ${item.title}`);
     clone.getElementById("productosTitle").textContent = item.title;
     clone.getElementById("productosInfo").textContent = item.description;
     clone.getElementById("addButton").addEventListener("click", function () {
@@ -117,23 +114,23 @@ const mostrarProductos = async (data) => {
 const verDetalle = async (item) => {
   const modal = document.getElementById("modalContainer");
   modal.classList.add("mostrar");
-  
+
   const cardDetalle = document.getElementById("detalleContainer");
   cardDetalle.innerHTML = "";
-  
+
   const templateDetalle = document.getElementById("templateDetalle").content;
   const fragment = document.createDocumentFragment();
 
   const clone = templateDetalle.cloneNode(true);
-  
+
   clone.getElementById("addToCart").addEventListener("click", function () {
     agregarCarrito(item);
   });
-  
+
   clone.getElementById("detalleImagen").setAttribute("src", item.images[0]);
   clone.getElementById("detalleNombre").textContent = item.title;
   clone.getElementById("detalleDescripcion").textContent = item.description;
-  
+
   clone.getElementById("detalleImagen1").setAttribute("src", item.images[1]);
   clone.getElementById("detalleImagen2").setAttribute("src", item.images[2]);
   clone.getElementById("detalleImagen3").setAttribute("src", item.images[3]);
@@ -151,7 +148,7 @@ function cerrarModal() {
 
 function agregarCarrito(item) {
   const productoExiste = carrito.find((producto) => producto.id === item.id);
-  
+
   if (productoExiste === undefined) {
     let productoCarrito = {
       posicion: carrito.length + 1,
@@ -160,14 +157,12 @@ function agregarCarrito(item) {
     };
     carrito.push(productoCarrito);
     sumarItems();
-    
-console.log(carrito)
 
+    console.log(carrito);
   } else {
     console.log("El producto ya existe en el carrito.");
   }
 }
-
 
 let contador = 0;
 
@@ -176,40 +171,32 @@ function sumarItems() {
   document.getElementById("cartItems").textContent = contador;
 }
 
+function abrirCarrito() {
+  document.getElementById("cartItems").addEventListener("click", () => {
+    const modal = document.getElementById("modalCarrito");
+    modal.classList.add("mostrarCarrito");
+  });
 
-function abrirCarrito(){
+  let tabla = "";
+  carrito.forEach((producto) => {
+    let fila = `<tr><td> ${producto.nombre} </td><td> ${producto.id}</td><td><button onclick="borrarIt(${producto.id})"> X </button></tr>`;
+    tabla += fila;
+  });
 
-document.getElementById("cartItems").addEventListener("click" , () => {
-  const modal = document.getElementById("modalCarrito");
-  modal.classList.add("mostrarCarrito");
-})
-
-let tabla = ""
-
-carrito.forEach(producto => {
-
-let fila = `<tr><td> HOLA MUNDO </td><td> ${producto.id}</td><td><button onclick="borrarIt(${producto.id})"> X </button></tr>`;
-tabla += fila;
-
-
-})
-
-
-document.getElementById("items").innerHTML = tabla
+  document.getElementById("items").innerHTML = tabla;
 }
 
-
-
-
-function borrarIt(item){
-  console.log("se borro el item" + item)
+function borrarIt(item) {
+  let newCarrito = carrito.filter((producto) => producto.id != item);
+  carrito = newCarrito;
+  abrirCarrito();
 }
 
-
-
-
-
-
+function vaciarCarrito(){
+  carrito = [];
+  abrirCarrito();
+  document.getElementById("items").innerText = "No hay productos en el Carrito.";
+}
 
 
 
@@ -225,7 +212,7 @@ function cerrarCarrito() {
 window.onclick = function (event) {
   const modalCarrito = document.getElementById("modalCarrito");
   let modal = document.getElementById("modalContainer");
-    
+
   if (event.target == modal) {
     modal.classList.remove("mostrar");
   }
