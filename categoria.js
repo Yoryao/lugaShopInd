bc61ef5a27ae7b193f51ed39ba16793133cc83ed
Fabrizio Signoretta <signorettafabrizio@gmail.com>
2022-05-18 22:51:46 -0300

    do responsive the layout

--- categoria.js
+++ categoria.js
@@ -1,374 +1,353 @@
 //VALIDAR NO SUMAR ITEMS YA EXISTENTES.
 //realizar toast para producto existente en el carrito
-console.log("Productos Funcionando.");
-
-(function () {
+console.log('Productos Funcionando.')
+;(function () {
   // https://dashboard.emailjs.com/admin/integration
-  emailjs.init("iMFHtbtT-GUb9TxOF");
-})();
-
-(function () {
+  emailjs.init('iMFHtbtT-GUb9TxOF')
+})()
+;(function () {
   // https://dashboard.emailjs.com/admin/integration
-  emailjs.init("iMFHtbtT-GUb9TxOF");
-})();
+  emailjs.init('iMFHtbtT-GUb9TxOF')
+})()
 
 //0)DECLARO VARIABLES A UTILIZAR
-let carrito = [
- 
-];
+let carrito = []
 
 //1) llamo a las categorias.
 window.onload = function () {
-  console.log("On Load Working");
-  loadCategorias();
-};
+  console.log('On Load Working')
+  loadCategorias()
+}
 
 //2) capturo las categorias
 const loadCategorias = async () => {
   try {
-    const res = await fetch("json/categorias.json");
-    const data = await res.json();
-    mostrarCategorias(data);
+    const res = await fetch('json/categorias.json')
+    const data = await res.json()
+    mostrarCategorias(data)
   } catch (error) {
-    console.log(error);
+    console.log(error)
   } finally {
-    console.log("finally del loadCategorias.");
+    console.log('finally del loadCategorias.')
   }
-};
+}
 
 //3) muestro las categorias
-const mostrarCategorias = async (data) => {
-  const cardCategoria = document.getElementById("categoriasContainer");
-  const template = document.getElementById("template").content;
-  const fragment = document.createDocumentFragment();
+const mostrarCategorias = async data => {
+  const cardCategoria = document.getElementById('categoriasContainer')
+  const template = document.getElementById('template').content
+  const fragment = document.createDocumentFragment()
 
-  data.forEach((item) => {
-    const clone = template.cloneNode(true);
+  data.forEach(item => {
+    const clone = template.cloneNode(true)
 
-    clone.getElementById(
-      "categoryButton"
-    ).style.backgroundImage = `url('${item.img}')`;
-    clone.getElementById("categoryP").textContent = item.nombre;
-    clone
-      .getElementById("categoryButton")
-      .addEventListener("click", function () {
-        elegirCategoria(item.nombre, item.id);
-      });
-
-    fragment.appendChild(clone);
-  });
-  cardCategoria.appendChild(fragment);
-};
+    clone.getElementById('category-img').src = item.img
+    clone.getElementById('categoryP').textContent = item.nombre
+    clone.getElementById('category-img').addEventListener('click', function () {
+      elegirCategoria(item.nombre, item.id)
+    })
+    fragment.appendChild(clone)
+  })
+  cardCategoria.appendChild(fragment)
+}
 
 //4) elijo la categoria.
-function elegirCategoria(id) {
-  console.log("Se ha clickeado categoria " + id);
+function elegirCategoria (id) {
+  console.log('Se ha clickeado categoria ' + id)
 
-  loadProductos(id);
+  loadProductos(id)
 }
 
 //5) cargar productos.
-const loadProductos = async (id) => {
+const loadProductos = async id => {
   try {
-    const res = await fetch("json/productos.json");
-    const catalogo = await res.json();
-    let filtrado = catalogo.filter((item) => item.category == id);
+    const res = await fetch('json/productos.json')
+    const catalogo = await res.json()
+    let filtrado = catalogo.filter(item => item.category == id)
 
-    if (id == undefined) {
-      mostrarProductos(catalogo);
+    if (!id) {
+      mostrarProductos(catalogo)
     } else {
-      mostrarProductos(filtrado);
+      mostrarProductos(filtrado)
     }
   } catch (error) {
-    console.log(error);
+    console.log(error)
   } finally {
-    console.log("finally del loadProductos.");
+    console.log('finally del loadProductos.')
   }
-};
-
-//6) mostrar Productos
-const mostrarProductos = async (data) => {
-  const cardProductos = document.getElementById("productosContainer");
-  cardProductos.innerHTML = "";
-
-  const templateProducto = document.getElementById("templateProductos").content;
-  const fragment = document.createDocumentFragment();
-
-
- 
-
-
-
-
-  data.forEach((item) => {
-    const clone = templateProducto.cloneNode(true);
-
-    clone.getElementById("productosImage").setAttribute("src", item.images[0]);
-    clone
-      .getElementById("productosImage")
-      .setAttribute("alt", `Photo of ${item.title}`)
-  
-    clone.getElementById("productosTitle").textContent = item.title;
-    clone.getElementById("productosInfo").textContent = item.description;
-    
-    clone.getElementById("addButton").addEventListener("click", function () {
-      verDetalle(item);
-    });
-
-    fragment.appendChild(clone);
-  });
-  cardProductos.appendChild(fragment);
-};
-
-
-
-const verDetalle = async (item) => {
-  console.log("Ver detalle " + item.title);
-  const modal = document.getElementById("modalDetalle");
-  modal.classList.add("mostrar");
-
-  const cardDetalle = document.getElementById("detalleContainer");
-  cardDetalle.innerHTML = "";
-
-  const templateDetalle = document.getElementById("templateDetalle").content;
-  const fragment = document.createDocumentFragment();
-
-  const clone = templateDetalle.cloneNode(true);
-
-  clone.getElementById("addToCart").addEventListener("click", function () {
-    agregarCarrito(item);
-    cerrarModal();
-  });
-
-  clone.getElementById("detalleImagen").setAttribute("src", item.images[0]);
-  clone.getElementById("detalleNombre").textContent = item.title;
-  clone.getElementById("detalleDescripcion").textContent = item.description;
-  clone.getElementById("detallePrice").textContent = item.price;
-  
-
-  fragment.appendChild(clone);
-
-  cardDetalle.appendChild(fragment);
-};
-
-function cerrarModal() {
-  let modal = document.getElementById("modalDetalle");
-  console.log("cerrando Modal");
-  modal.classList.remove("mostrar");
 }
 
-function agregarCarrito(item) {
-  const productoExiste = carrito.find((producto) => producto.id === item.id);
-  console.log("se Agrego al Carrito");
+//6) mostrar Productos
+const mostrarProductos = async data => {
+  const cardProductos = document.getElementById('productosContainer')
+  cardProductos.innerHTML = ''
+
+  const templateProducto = document.getElementById('templateProductos').content
+  const fragment = document.createDocumentFragment()
+
+  data.forEach(item => {
+    const clone = templateProducto.cloneNode(true)
+    clone
+      .getElementById('productosImage')
+      .addEventListener('click', function () {
+        verDetalle(item)
+      })
+    clone.getElementById('productosImage').setAttribute('src', item.images[0])
+    clone
+      .getElementById('productosImage')
+      .setAttribute('alt', `Photo of ${item.title}`)
+
+    clone.getElementById('productosTitle').textContent = item.title
+    clone.getElementById('productosInfo').textContent = item.description
+
+    fragment.appendChild(clone)
+  })
+  cardProductos.appendChild(fragment)
+}
+
+const verDetalle = async item => {
+  console.log('Ver detalle ' + item.title)
+  const modal = document.getElementById('modalDetalle')
+  modal.classList.add('mostrar')
+
+  const cardDetalle = document.getElementById('detalleContainer')
+  cardDetalle.innerHTML = ''
+
+  const templateDetalle = document.getElementById('templateDetalle').content
+  const fragment = document.createDocumentFragment()
+
+  const clone = templateDetalle.cloneNode(true)
+
+  clone.getElementById('addToCart').addEventListener('click', function () {
+    agregarCarrito(item)
+    cerrarModal()
+  })
+
+  clone.getElementById('detalleImagen').setAttribute('src', item.images[0])
+  clone.getElementById('detalleNombre').textContent = item.title
+  clone.getElementById('detalleDescripcion').textContent = item.description
+  clone.getElementById('detallePrice').textContent = item.price
+
+  fragment.appendChild(clone)
+
+  cardDetalle.appendChild(fragment)
+}
+
+function cerrarModal () {
+  let modal = document.getElementById('modalDetalle')
+  console.log('cerrando Modal')
+  modal.classList.remove('mostrar')
+}
+
+function agregarCarrito (item) {
+  const productoExiste = carrito.find(producto => producto.id === item.id)
+  console.log('se Agrego al Carrito')
   if (productoExiste === undefined) {
     let productoCarrito = {
       posicion: carrito.length + 1,
       nombre: item.title,
-      id: item.id,
-    };
-    carrito.push(productoCarrito);
-    sumarItems();
-    cerrarCarrito();
+      id: item.id
+    }
+    carrito.push(productoCarrito)
+    sumarItems()
+    cerrarCarrito()
     Toastify({
-      text: "El producto se agrego al carrito.",
+      text: 'El producto se agrego al carrito.',
       duration: 1500,
-      gravity: "top", // `top` or `bottom`
-      position: "center", // `left`, `center` or `right`
+      gravity: 'top', // `top` or `bottom`
+      position: 'center', // `left`, `center` or `right`
       stopOnFocus: true, // Prevents dismissing of toast on hover
       style: {
-        background: "pink",
-        borderRadius: "5px",
-        color: "black",
-      },
-    }).showToast();
+        background: 'pink',
+        borderRadius: '5px',
+        color: 'black'
+      }
+    }).showToast()
   } else {
-    console.log("El producto ya existe en el carrito.");
+    console.log('El producto ya existe en el carrito.')
     Toastify({
-      text: "El producto ya existe en el carrito.",
+      text: 'El producto ya existe en el carrito.',
       duration: 1500,
-      gravity: "top", // `top` or `bottom`
-      position: "center", // `left`, `center` or `right`
+      gravity: 'top', // `top` or `bottom`
+      position: 'center', // `left`, `center` or `right`
       stopOnFocus: true, // Prevents dismissing of toast on hover
       style: {
-        background: "pink",
-        borderRadius: "5px",
-        color: "black",
-      },
-    }).showToast();
+        background: 'pink',
+        borderRadius: '5px',
+        color: 'black'
+      }
+    }).showToast()
   }
 }
 
-let contador = 0;
+let contador = 0
 
-function sumarItems() {
-  contador++;
-  document.getElementById("cartItems").textContent = contador;
+function sumarItems () {
+  contador++
+  document.getElementById('cartItems').textContent = contador
 }
 
-function restarItems() {
-  contador--;
-  document.getElementById("cartItems").textContent = contador;
+function restarItems () {
+  contador--
+  document.getElementById('cartItems').textContent = contador
 }
 
-function resetItems() {
-  contador = 0;
-  document.getElementById("cartItems").textContent = contador;
+function resetItems () {
+  contador = 0
+  document.getElementById('cartItems').textContent = contador
 }
 
-function abrirCarrito() {
-  const modal = document.getElementById("modalCarrito");
-  modal.classList.add("mostrarCarrito");
+function abrirCarrito () {
+  const modal = document.getElementById('modalCarrito')
+  modal.classList.add('mostrarCarrito')
 
-  let tabla = "";
-  carrito.forEach((producto) => {
-    let fila = `<tr><td> ${producto.id} </td><td> ${producto.nombre}</td><td><button onclick="borrarIt(${producto.id})" class="borrarBtn"> X </button></tr>`;
-    tabla += fila;
-  });
+  let tabla = ''
+  carrito.forEach(producto => {
+    let fila = `<tr><td> ${producto.id} </td><td> ${producto.nombre}</td><td><button onclick="borrarIt(${producto.id})" class="borrarBtn"> X </button></tr>`
+    tabla += fila
+  })
 
-  document.getElementById("items").innerHTML = tabla;
+  document.getElementById('items').innerHTML = tabla
 }
 
-function borrarIt(item) {
+function borrarIt (item) {
   Toastify({
     text: `Se borro el item ${item}.`,
     duration: 1500,
-    gravity: "top", // `top` or `bottom`
-    position: "center", // `left`, `center` or `right`
+    gravity: 'top', // `top` or `bottom`
+    position: 'center', // `left`, `center` or `right`
     stopOnFocus: true, // Prevents dismissing of toast on hover
     style: {
-      background: "pink",
-      borderRadius: "5px",
-      color: "black",
-    },
-  }).showToast();
-  let newCarrito = carrito.filter((producto) => producto.id != item);
-  carrito = newCarrito;
-  restarItems();
-  abrirCarrito();
+      background: 'pink',
+      borderRadius: '5px',
+      color: 'black'
+    }
+  }).showToast()
+  let newCarrito = carrito.filter(producto => producto.id != item)
+  carrito = newCarrito
+  restarItems()
+  abrirCarrito()
 
   if (carrito.length == 0) {
-    vaciarCarrito();
+    vaciarCarrito()
   }
 }
 
-function vaciarCarrito() {
-  carrito = [];
-  resetItems();
-  abrirCarrito();
-  document.getElementById("items").innerText =
-    "No hay productos en el Carrito.";
+function vaciarCarrito () {
+  carrito = []
+  resetItems()
+  abrirCarrito()
+  document.getElementById('items').innerText = 'No hay productos en el Carrito.'
 }
 
-function mostrarFormulario() {
-  let textoForm = document.getElementById("textoForm");
-  textoForm.value = "Pedido";
+function mostrarFormulario () {
+  let textoForm = document.getElementById('textoForm')
+  textoForm.value = 'Pedido'
 
-  const modal = document.getElementById("modalFormulario");
-  modal.classList.add("mostrarFormulario");
+  const modal = document.getElementById('modalFormulario')
+  modal.classList.add('mostrarFormulario')
 
-  textoPedido = "Hola Lucia, estoy interesada en los siguientes productos: ";
+  textoPedido = 'Hola Lucia, estoy interesada en los siguientes productos: '
 
   carrito.forEach(
-    (producto) =>
+    producto =>
       (textoPedido += `${producto.nombre}, con codigo ${producto.id}. `)
-  );
+  )
 
-  document.getElementById("textoForm");
-  textoForm.value = textoPedido;
-  console.log("se mostro el formulario." + textoPedido);
-  cerrarCarrito();
+  document.getElementById('textoForm')
+  textoForm.value = textoPedido
+  console.log('se mostro el formulario.' + textoPedido)
+  cerrarCarrito()
 }
 
-function enviarFormulario() {
+function enviarFormulario () {
   document
-    .getElementById("contact-form")
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
-      console.log("se envio formulario");
+    .getElementById('contact-form')
+    .addEventListener('submit', function (event) {
+      event.preventDefault()
+      console.log('se envio formulario')
       // generate a five digit number for the contact_number variable
-      this.contact_number.value = (Math.random() * 100000) | 0;
+      this.contact_number.value = (Math.random() * 100000) | 0
       // these IDs from the previous steps
-      emailjs.sendForm("contact_service", "contact_form", this).then(
+      emailjs.sendForm('contact_service', 'contact_form', this).then(
         function () {
           Toastify({
-            text: "Se envio el pedido, en breve nos comunicaremos con vos.",
+            text: 'Se envio el pedido, en breve nos comunicaremos con vos.',
             duration: 1500,
-            gravity: "top", // `top` or `bottom`
-            position: "center", // `left`, `center` or `right`
+            gravity: 'top', // `top` or `bottom`
+            position: 'center', // `left`, `center` or `right`
             stopOnFocus: true, // Prevents dismissing of toast on hover
             style: {
-              background: "pink",
-              borderRadius: "5px",
-              color: "black",
-            },
-          }).showToast();
+              background: 'pink',
+              borderRadius: '5px',
+              color: 'black'
+            }
+          }).showToast()
 
-          vaciarCarrito();
-          cerrarCarrito();
-          cerrarFormulario();
+          vaciarCarrito()
+          cerrarCarrito()
+          cerrarFormulario()
         },
         function (error) {
-          console.log("FAILED...", error);
+          console.log('FAILED...', error)
         }
-      );
-    });
+      )
+    })
 }
 
-function addNewsletter() {
+function addNewsletter () {
   document
-    .getElementById("newsletter-form")
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
-      console.log("Se envio Correo");
+    .getElementById('newsletter-form')
+    .addEventListener('submit', function (event) {
+      event.preventDefault()
+      console.log('Se envio Correo')
       // these IDs from the previous steps
-      emailjs.sendForm("contact_service", "add_newsletter", this).then(
+      emailjs.sendForm('contact_service', 'add_newsletter', this).then(
         function () {
           Toastify({
-            text: "{Te suscribiste al newsletter.}.",
+            text: '{Te suscribiste al newsletter.}.',
             duration: 1500,
-            gravity: "top", // `top` or `bottom`
-            position: "center", // `left`, `center` or `right`
+            gravity: 'top', // `top` or `bottom`
+            position: 'center', // `left`, `center` or `right`
             stopOnFocus: true, // Prevents dismissing of toast on hover
             style: {
-              background: "pink",
-              borderRadius: "5px",
-              color: "black",
-            },
-          }).showToast();
-
-
+              background: 'pink',
+              borderRadius: '5px',
+              color: 'black'
+            }
+          }).showToast()
         },
         function (error) {
-          console.log("FAILED...", error);
+          console.log('FAILED...', error)
         }
-      );
-    });
+      )
+    })
 }
 
-function cerrarFormulario() {
-  const modal = document.getElementById("modalFormulario");
-  console.log("cerrando Modal");
-  modal.classList.remove("mostrarFormulario");
+function cerrarFormulario () {
+  const modal = document.getElementById('modalFormulario')
+  console.log('cerrando Modal')
+  modal.classList.remove('mostrarFormulario')
 }
 
-function cerrarCarrito() {
-  const modal = document.getElementById("modalCarrito");
-  console.log("cerrando Modal");
-  modal.classList.remove("mostrarCarrito");
+function cerrarCarrito () {
+  const modal = document.getElementById('modalCarrito')
+  console.log('cerrando Modal')
+  modal.classList.remove('mostrarCarrito')
 }
 
 window.onclick = function (event) {
-  const modalCarrito = document.getElementById("modalCarrito");
-  const modal = document.getElementById("modalDetalle");
-  const modalFormulario = document.getElementById("modalFormulario");
+  const modalCarrito = document.getElementById('modalCarrito')
+  const modal = document.getElementById('modalDetalle')
+  const modalFormulario = document.getElementById('modalFormulario')
 
   if (event.target == modal) {
-    modal.classList.remove("mostrar");
+    modal.classList.remove('mostrar')
   }
   if (event.target == modalCarrito) {
-    modalCarrito.classList.remove("mostrarCarrito");
+    modalCarrito.classList.remove('mostrarCarrito')
   }
   if (event.target == modalFormulario) {
-    modalFormulario.classList.remove("mostrarFormulario");
+    modalFormulario.classList.remove('mostrarFormulario')
   }
-};
+}
